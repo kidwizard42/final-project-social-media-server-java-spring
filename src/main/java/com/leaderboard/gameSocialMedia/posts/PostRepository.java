5b2e1dcee0160d42fac054c39544723165f2759a
Darwood Antoine <77207031+kidwizard42@users.noreply.github.com>
2022-04-04 11:08:28 -0400

    Delete src directory

--- src/main/java/com/leaderboard/gameSocialMedia/posts/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.leaderboard.gameSocialMedia.posts;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Integer> {
-
-}
--- src/main/java/com/leaderboard/gameSocialMedia/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leaderboard.gameSocialMedia.users;
-
-import com.leaderboard.gameSocialMedia.users.Users;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Users, Integer> {
-    public Users findByUsername(String username);
-    Users grabPassword(String username);
-}
